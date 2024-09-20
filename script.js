7a4d1461bd4d98e87c6442028e2717d632a0d080
Mohd Hamdaan Ansari <mhamdaan91@gmail.com>
2024-09-20 23:56:32 +0530

    Currency Converter

---
+++ script.js
@@ -0,0 +1,58 @@
+const API_KEY = "cur_live_OnazRTSLUcQUM0gsjhLswsnepyZ3ci9RlYMJiCHW";
+const BASE_URL = `https://api.currencyapi.com/v3/latest?apikey=${API_KEY}`;
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected"; 
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const fromCurrency = fromCurr.value;
+    const toCurrency = toCurr.value;
+
+    fetch(`${BASE_URL}&base_currency=${fromCurrency}&currencies=${toCurrency}`)
+        .then(response => response.json())
+        .then(data => {
+            let exchangeRate = data.data[toCurrency].value;
+            let convertedAmount = (amtVal * exchangeRate).toFixed(2);
+            msg.innerText = `${amtVal} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        })
+        .catch(error => {
+            msg.innerText = "Error fetching exchange rate.";
+            console.error("Error:", error);
+        });
+});
